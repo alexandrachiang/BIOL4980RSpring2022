b14877153c49d6b973338b5bfb80b30c8b62f6a9
Alex Chiang <76176370+alexandrachiang@users.noreply.github.com>
2022-05-03 04:54:49 -0400

    Create combineGWIS.sh

---
+++ combineGWIS.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --job-name=combineGWIS
+#SBATCH --ntasks=1
+#SBATCH --time=4:00:00
+#SBATCH --mem=20
+#SBATCH --output=combineGWIS.%j.out
+#SBATCH --error=combineGWIS.%j.err
+#SBATCH --mail-user=ahc87874@uga.edu
+#SBATCH --mail-type=ALL
+
+cd $SLURM_SUBMIT_DIR
+
+module load R/4.1.3-foss-2020b
+R CMD BATCH combineGWIS.R
